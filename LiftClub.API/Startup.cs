86a57478a7df7fdad10b9440239feed0fec19a93
Cameron Smith <cameronsmith63@gmail.com>
2020-05-12 09:12:42 +0200

    Added Swagger

--- LiftClub.API/Startup.cs
+++ LiftClub.API/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+// (1) Swagger namespace
+using Microsoft.OpenApi.Models;
+
 namespace LiftClub.API
 {
     public class Startup
@@ -26,11 +29,48 @@ namespace LiftClub.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // (2) Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Lift Club API",
+                    Description = "An ASP.NET Core Web API for a Lift Club application",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Cameron Smith",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/CameronRSmith85"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // (3) Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // (4) Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                // JSON Endpoint: https://localhost:44313/swagger/v1/swagger.json
+                // Swagger UI: https://localhost:44313/swagger/index.html
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lift Club V1");
+                // Serving the Swagger UI at the app's root
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
